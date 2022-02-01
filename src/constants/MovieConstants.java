9f4bf75d1f978c930351aeb670273ce896edbf87
Gabi iezzi <geiezzi90@gmail.com>
2022-02-01 18:47:41 -0300

    Segundo commit

--- src/constants/MovieConstants.java
+++ src/constants/MovieConstants.java
@@ -13,7 +13,7 @@ public final class MovieConstants implements java.io.Serializable {
 
     private static final long serialVersionUID = -4934481031495866551L;
 
-    private static final String MENU_MESSAGE = "---	Menu	---\n"
+    public static final String MENU_MESSAGE = "---	Menu	---\n"
             + "\n"
             + "1.	Add a movie.\n"
             + "2.	Show all the movies.\n"
--- src/implement/MovieServiceImplement.java
+++ src/implement/MovieServiceImplement.java
@@ -8,18 +8,20 @@ package implement;
 import constants.MovieConstants;
 import entity.MovieEntity;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.Scanner;
 import service.MovieService;
+import utilities.MovieComparators;
 
 /**
  *
  * @author Gabi
  */
 public class MovieServiceImplement implements MovieService {
-    
+
     Scanner read;
-    
+
     ArrayList<MovieEntity> movieList;
 
     public MovieServiceImplement(Scanner read, ArrayList<MovieEntity> movieList) {
@@ -27,104 +29,167 @@ public class MovieServiceImplement implements MovieService {
         this.movieList = new ArrayList();
     }
 
-    
-    
-    
-    
     @Override
-     public MovieEntity newMovie(){
-         
+    public MovieEntity newMovie() {
+
         MovieEntity newMovie = new MovieEntity();
-        
+
         System.out.println("Insert the movie name!");
         newMovie.setMovie(read.next());
         System.out.println("Insert the movie director!");
         newMovie.setDirector(read.next());
         System.out.println("Insert the movie duration!");
         newMovie.setMovie(read.next());
-         
-         return newMovie;
-     }
-     
-    @Override 
-    public String addMovieToTheList(MovieEntity newMovie){
-        
-        movieList.add(newMovie);
-        return newMovie.getMovie()+ " ,"+ newMovie.getDirector()+ " ,"+ newMovie.getDuration()+ ":" + " ADDED to the list" ;
-        
+
+        return newMovie;
     }
-    
+
     @Override
-    public String showAllTheMovies(){
-        
+    public String addMovieToTheList(MovieEntity newMovie) {
+
+        movieList.add(newMovie);
+        return newMovie.getMovie() + " ," + newMovie.getDirector() + " ," + newMovie.getDuration() + ":" + " ADDED to the list";
+
+    }
+
+    @Override
+    public String showAllTheMovies() {
+
         String allMovies = "All Movies: ";
         for (MovieEntity unitary : movieList) {
-            allMovies += "\n"+ unitary.toString();
+            allMovies += "\n" + unitary.toString();
         }
-        
+
         return allMovies;
     }
-    
+
     @Override
-    public String showMoviesOfMoreThanOneHour(){
-        
+    public String showMoviesOfMoreThanOneHour() {
+
         Iterator<MovieEntity> it = movieList.iterator();
         String moreThanOneHourMovie = " Movies from more than 1 hour: \n";
-        while(it.hasNext()){
-            
-            if(it.next().getDuration()>=60){
-                
+        while (it.hasNext()) {
+
+            if (it.next().getDuration() >= 60) {
+
                 moreThanOneHourMovie += it.next().toString();
-                
+
             }
-            
+
         }
-        
+
         return moreThanOneHourMovie;
     }
-    
+
     @Override
-    public String SortMovieLongerToShorterDuration(){
-        
-        return null;
+    public String SortMovieLongerToShorterDuration() {
+
+        Collections.sort(movieList, MovieComparators.sortByMovieDuration);
+
+        String sortedByDuration = "Movie Sorted by Duration: \n";
+
+        for (MovieEntity unitary : movieList) {
+
+            sortedByDuration += unitary;
+
+        }
+
+        return sortedByDuration;
     }
-    
+
     @Override
-    public String SortMovieShorterToLongerDuration(){
-        
-        return null;
+    public String SortMovieShorterToLongerDuration() {
+
+        Collections.sort(movieList, MovieComparators.sortByMovieName);
+
+        String sortedByName = " Movies Sorted by Name : \n";
+
+        for (MovieEntity unitary : movieList) {
+
+            sortedByName += unitary;
+
+        }
+
+        return sortedByName;
+
     }
-    
+
     @Override
-    public String SortMoviesAlphabeticallyByName(){
-        
-        return null;
+    public String SortMoviesAlphabeticallyByName() {
+
+        Collections.sort(movieList, Collections.reverseOrder(MovieComparators.sortByMovieDuration) );
+
+        String sortedByName = " Movies Sorted by Name : \n";
+
+        for (MovieEntity unitary : movieList) {
+
+            sortedByName += unitary;
+
+        }
+
+        return sortedByName;
     }
-    
+
     @Override
-    public String SortMoviesAlphabeticallyByDirector(){
-        
-        return null;
+    public String SortMoviesAlphabeticallyByDirector() {
+
+        Collections.sort(movieList, MovieComparators.sortByMovieDirector);
+
+        String sortedByDirector = "Movies Sorted By Director : \n";
+
+        for (MovieEntity unitary : movieList) {
+
+            sortedByDirector += unitary;
+
+        }
+
+        return sortedByDirector;
     }
-    
+
     @Override
-    public void menu(){
-       
+    public void menu() {
+
         int option = 0;
-        
+
         do {
-         
-            constans.MENU_MESSAGE
-            
+
+            System.out.println(MovieConstants.MENU_MESSAGE);
+
             option = read.nextInt();
             
-            
-            
-            
-        } while (option != );
-        
-        
-        
-        
+            switch (option) {
+                case 1:
+                    
+                    break;
+               
+                case 2:
+                    
+                    break;
+                
+                case 3:
+                    
+                    break;
+               
+                case 4:
+                    
+                    break;
+                
+                case 5:
+                    
+                    break;
+                
+                case 6:
+                    
+                    break;
+                
+                case 7:
+                    
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+
+        } while (option != 8);
+
     }
 }
